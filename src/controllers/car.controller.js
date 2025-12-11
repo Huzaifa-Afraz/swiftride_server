bcd4238e07912df373c45dabe0a35f314d307c41
DevBasitali <rajaalix2022@gmail.com>
2025-12-11 21:04:59 +0500

    basit

--- src/controllers/car.controller.js
+++ src/controllers/car.controller.js
@@ -1,119 +1,40 @@
-// import httpStatus from "http-status";
-// import * as carService from "../services/car.service.js";
-// import { sendSuccessResponse } from "../utils/response.js";
-// import catchAsync from "../utils/catchAsync.js";
-
-// export const createCar = catchAsync(async (req, res) => {
-//   const ownerId = req.user.id;
-//   const payload = req.body;
-//   const files = req.files?.images || [];
-
-//   const car = await carService.createCar(ownerId, payload, files);
-
-//   sendSuccessResponse(res, httpStatus.CREATED, "Car listed successfully", {
-//     car
-//   });
-// });
-
 import httpStatus from "http-status";
 import catchAsync from "../utils/catchAsync.js";
-import * as carService from "../services/car.service.js";
 import { sendSuccessResponse } from "../utils/response.js";
+import * as carService from "../services/car.service.js"; // Import service
 
-export const createCar = async (ownerId, ownerRole, payload) => {
-  if (![USER_ROLE.HOST, USER_ROLE.SHOWROOM].includes(ownerRole)) {
-    throw new ApiError(
-      httpStatus.FORBIDDEN,
-      "Only hosts or showrooms can list cars"
-    );
-  }
+export const createCar = catchAsync(async (req, res) => {
+  const ownerId = req.user.id;
+  const ownerRole = req.user.role;
+  
+  // Extract images from Multer
+  const images = req.files?.photos || [];
+  
+  // Prepare data for service
+  const carData = {
+    ...req.body,
+    photos: images.map(file => file.path) // Store file paths
+  };
 
-  const existingCount = await Car.countDocuments({ owner: ownerId });
-  if (existingCount >= MAX_CARS_PER_USER) {
-    throw new ApiError(
-      httpStatus.BAD_REQUEST,
-      `You have reached the maximum car limit (${MAX_CARS_PER_USER})`
-    );
-  }
+  const car = await carService.createCar(ownerId, ownerRole, carData);
 
-  const {
-    make,
-    model,
-    year,
-    color,
-    plateNumber,
-    pricePerHour,
-    pricePerDay,
-    seats,
-    transmission,
-    fuelType,
-    photos,
-    location,
-    availability,
-    features
-  } = payload;
-
-  const car = await Car.create({
-    owner: ownerId,
-    ownerRole,
-    make,
-    model,
-    year,
-    color,
-    plateNumber,
-    pricePerHour,
-    pricePerDay,
-    seats,
-    transmission,
-    fuelType,
-    photos: photos || [],
-    location,
-    availability,
-    features: features || []
-  });
-
-  return car;
-};
-
+  sendSuccessResponse(res, httpStatus.CREATED, "Car listed successfully", { car });
+});
 
 export const getMyCars = catchAsync(async (req, res) => {
-  const ownerId = req.user.id;
-
-  const cars = await carService.getMyCars(ownerId);
-
-  sendSuccessResponse(res, httpStatus.OK, "Your cars fetched successfully", {
-    cars
-  });
+  const cars = await carService.getMyCars(req.user.id);
+  sendSuccessResponse(res, httpStatus.OK, "Your cars fetched successfully", { cars });
 });
 
 export const searchCars = catchAsync(async (req, res) => {
-  const {
-    page,
-    limit,
-    brand,
-    model,
-    location,
-    minPrice,
-    maxPrice,
-    minYear,
-    maxYear
-  } = req.query;
-
-  const result = await carService.searchCars(
-    {
-      brand,
-      model,
-      location,
-      minPrice: minPrice ? Number(minPrice) : undefined,
-      maxPrice: maxPrice ? Number(maxPrice) : undefined,
-      minYear: minYear ? Number(minYear) : undefined,
-      maxYear: maxYear ? Number(maxYear) : undefined
-    },
-    {
-      page: page ? Number(page) : 1,
-      limit: limit ? Number(limit) : 10
-    }
-  );
-
+  const result = await carService.searchCars(req.query);
   sendSuccessResponse(res, httpStatus.OK, "Cars fetched successfully", result);
+});
+
+export const getCarById = catchAsync(async (req, res) => {
+  const car = await carService.getCarById(req.params.id);
+  if (!car) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Car not found");
+  }
+  sendSuccessResponse(res, httpStatus.OK, "Car fetched successfully", { car });
 });
