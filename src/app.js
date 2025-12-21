9b785aa6f9ddc19a85b75727fde4d32780eb858e
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2025-12-21 18:50:56 +0500

    update cors

--- src/app.js
+++ src/app.js
@@ -1,55 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import helmet from "helmet";
-// import morgan from "morgan";
-// import cookieParser from "cookie-parser";
-
-// import apiRoutes from "./routes/index.js";
-
-
-// import { notFoundHandler, errorHandler } from "./middlewares/error.middleware.js";
-
-
-
-// const app = express();
-
-
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "http://localhost:3000",
-// ];
-
-// app.use(
-//   cors({
-//     origin: allowedOrigins,
-//     credentials: true,
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"]
-//   })
-// );
-// // Middlewares
-// app.use(helmet());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-// app.use(morgan("dev"));
-
-// // Health check
-// app.get("/api/health", (req, res) => {
-//   res.json({ status: "ok", service: "swiftride-api" });
-// });
-
-// // API routes
-// app.use("/api", apiRoutes);
-
-// // 404 + error handlers
-// app.use(notFoundHandler);
-// app.use(errorHandler);
-
-// export default app;
-
-
-
 //=====================
 // below is the code for vercel deployment
 //=====================
@@ -69,8 +17,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  // 2. ADD YOUR FUTURE VERCEL FRONTEND URL HERE LATER
-  "https://swiftride-frontend.vercel.app" 
+  "https://swiftride-frontend.vercel.app"
 ];
 
 app.use(
@@ -100,7 +47,7 @@ app.use(async (req, res, next) => {
 });
 
 app.get("/api/health", (req, res) => {
-  res.json({ status: "ok", service: "swiftride-api" });
+  res.json({ status: "ok", service: "swiftride-api 0.0.0" });
 });
 
 app.use("/api", apiRoutes);
