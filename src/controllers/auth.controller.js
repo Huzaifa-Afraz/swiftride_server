77e8ef0aa0558d5a70df8378b6130de8d8464b9d
DevBasitali <rajaalix2022@gmail.com>
2025-12-23 20:40:41 +0500

    fix: The LaxSite is now set to true

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -199,7 +199,7 @@ export const googleLogin = async (req, res, next) => {
       res.cookie("token", token?.accessToken, {
     httpOnly: true,
     secure: true, 
-    sameSite: "true",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
   });
